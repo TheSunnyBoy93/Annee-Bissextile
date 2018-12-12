32ef08728f605de3417e4c2652830456af1944f4
SunnyBoy <45802098+TheSunnyBoy93@users.noreply.github.com>
2018-12-13 00:18:36 +0100

    Add files via upload

---
+++ Bisextille.py
@@ -0,0 +1,8 @@
+annee=input("Saisissez une année : ")
+annee=int(annee)
+
+if annee%400 == 0 or (annee%4 == 0 and annee%100 !=0):
+	print("L'année", annee,"est bisextille")
+else:
+	print("L'année", annee,"n'est pas bisextille")
+	
